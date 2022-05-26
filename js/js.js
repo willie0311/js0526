cb22d615b6e91a9590d9c04614fffd05e27db252
willie <willie@gmail.com>
2022-05-26 14:33:38 +0800

    for(1)

--- js/js.js
+++ js/js.js
@@ -76,8 +76,25 @@
 // }
 
 // 最少程式會執行一次
-let i = 0;
-do {
-    i++;
-    document.write(i + ' ');
-}while(i<10)
+// let i = 0;
+// do {
+//     i++;
+//     document.write(i + ' ');
+// }while(i<10)
+
+let countI = 0;
+for( i=1 ; i<=10 ; i++ ){
+    countI = countI + i;
+    
+    // 1  =  0  1次
+    // 3  =  1  2次
+    // 6  =  3  3次
+    // 10 =  6  4次
+    // 15 = 10  5次
+    // 21 = 15  6次
+    // 28 = 21  7次
+    // 36 = 28  8次
+    // 45 = 36  9次
+    // 55 = 45  10次
+}
+document.write(countI);
