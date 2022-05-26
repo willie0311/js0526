951814597deea53e00161a8531f2232decbcafea
willie <willie@gmail.com>
2022-05-26 11:36:28 +0800

    switch

--- js/js.js
+++ js/js.js
@@ -19,27 +19,42 @@
 
 // (a>=0)?document.write('數字為正數'):document.write('數字為負數');
 
-let score = prompt('請輸入分數','0');
-score = Number(score);
-// (score = NaN) 下面語法可以寫成這樣
-if(isNaN(score)){  
-    alert('請輸入阿拉伯數字')
-}else{
-    if (score<0){
-        alert('請輸入大於0的數字');
-    }else if(score>=0 && score<60){
-        document.write('不及格');
-    }else if(score>=60 && score<70){
-        document.write('丙等');
-    }else if(score>=70 && score<80){
-        document.write('乙等');
-    }else if(score>=80 && score<90){
-        document.write('甲等');
-    }else if(score>=90 && score<=100){
-        document.write('優等');
-    }else{
-        alert('請輸入小於100的數字');
-    }
+// let score = prompt('請輸入分數','0');
+// score = Number(score);
+// // (score = NaN) 下面語法可以寫成這樣
+// if(isNaN(score)){  
+//     alert('請輸入阿拉伯數字')
+// }else{
+//     if (score<0){
+//         alert('請輸入大於0的數字');
+//     }else if(score>=0 && score<60){
+//         document.write('不及格');
+//     }else if(score>=60 && score<70){
+//         document.write('丙等');
+//     }else if(score>=70 && score<80){
+//         document.write('乙等');
+//     }else if(score>=80 && score<90){
+//         document.write('甲等');
+//     }else if(score>=90 && score<=100){
+//         document.write('優等');
+//     }else{
+//         alert('請輸入小於100的數字');
+//     }
+// }
+
+
+let payway = prompt('請輸入付款方式:1.ATM 2.刷卡 3.貨到付款','1');
+
+switch (payway){
+    case "1" :
+        document.write('付款方式為ATM匯款');
+        break;
+    case "2" :
+        document.write('付款方式為刷卡');
+        break;
+    case "3" :
+        document.write('付款方式為貨到付款');
+        break;
+    default :
+        document.write('請選擇正確的付款方式');
 }
-
-
