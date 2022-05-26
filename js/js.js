82ef131727c49e588c68b1eb2b6be32ca29693fc
willie <willie@gmail.com>
2022-05-26 13:46:22 +0800

    while 迴圈

--- js/js.js
+++ js/js.js
@@ -43,27 +43,34 @@
 // }
 
 
-let score = prompt('請輸入分數',);
-score = Number(score);
-switch (true){
-    case score<0 :
-        alert('請輸入大於0的數字');
-        break;
-    case score>=0 && score<60 :
-        document.write('不及格');
-        break;
-    case score>=60 && score<70 :
-        document.write('丙等');
-        break;
-    case score>=70 && score<80 :
-        document.write('乙等');
-        break;
-    case score>=80 && score<90 :
-        document.write('甲等');
-        break;
-    case score>=90 && score<=100 :
-        document.write('優等');
-        break;
-    default :
-        alert('請輸入小於100的數字');
+// let score = prompt('請輸入分數',);
+// score = Number(score);
+// switch (true){
+//     case score<0 :
+//         alert('請輸入大於0的數字');
+//         break;
+//     case score>=0 && score<60 :
+//         document.write('不及格');
+//         break;
+//     case score>=60 && score<70 :
+//         document.write('丙等');
+//         break;
+//     case score>=70 && score<80 :
+//         document.write('乙等');
+//         break;
+//     case score>=80 && score<90 :
+//         document.write('甲等');
+//         break;
+//     case score>=90 && score<=100 :
+//         document.write('優等');
+//         break;
+//     default :
+//         alert('請輸入小於100的數字');
+// }
+
+
+let i= 0;
+while (i<10){
+    i++;
+    document.write(i + ' ');
 }
