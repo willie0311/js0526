12b8740bdd9d9adb28e240cc023fb01e8a3e3b29
willie <willie@gmail.com>
2022-05-26 09:29:54 +0800

    if else版本

---
+++ js/js.js
@@ -0,0 +1,27 @@
+// let text0 ="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quisquam quo eius quod architecto, aut fuga maiores reiciendis officia voluptates animi facere! Accusamus, perspiciatis. Facilis magnam totam amet fugit eum?";
+
+// let text ="Lorem";
+// console.log(text[0]);
+// console.log(text[1]);
+// console.log(text[2]);
+// console.log(text[3]);
+// console.log(text[4]);
+
+
+// console.log(text0.slice(10,16));
+// console.log(text0.slice(-15,-3));
+
+
+// let text1 ="quo eius quod architecto";
+// console.log(text1.indexOf("s"));
+// console.log(text1.indexOf("S"));
+// console.log(text1.split(""));
+
+
+
+let a = prompt('請輸入數字','0');
+if (a>=0){
+    document.write('數字為正數')
+}else{
+    document.write('數字為負數')
+}
