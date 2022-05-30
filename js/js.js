aab7658e581aca06766b23b7750e0983b2ac278a
willie <willie@gmail.com>
2022-05-30 10:56:32 +0800

    array

--- js/js.js
+++ js/js.js
@@ -105,32 +105,42 @@
 // let pdc = prompt('請輸入攝氏溫度:','25');
 // document.write('華氏溫度為:'+ convertDF(pdc))
 
-function sort(nums){
-    //找出未排序中最小值
-function minIndex(left,right){
-if(right === nums.length){
-    return left;
-}else if(nums[right] < nums[left]){
-    return minIndex(right,right+1);
-}else{
-    return minIndex(left,right+1);
+// function sort(nums){
+//     //找出未排序中最小值
+// function minIndex(left,right){
+// if(right === nums.length){
+//     return left;
+// }else if(nums[right] < nums[left]){
+//     return minIndex(right,right+1);
+// }else{
+//     return minIndex(left,right+1);
+// }
+// } 
+
+
+
+// for(let i= 0;i<nums.length;i++){
+//     let selected = minIndex(i,i+1);
+//     if(i !== selected){
+//         //交換i與selected處的元素
+//         //換位子的程式
+//         let tmp = nums[i];
+//         nums[i] = nums[selected];
+//         nums[selected] = tmp;
+//     }
+// }
+// }
+
+// let nums = [10 ,3 ,5 ,2 ,4 ];
+// sort(nums);
+// console.log(nums);
+
+
+//array
+
+let student = ["willie","siang","abby"];
+document.write('<table border="1"><tr><td>編號</td><td>姓名</td></tr>');
+for (let i = 0;i <student.length;i++){
+    document.write('<tr><td>'+(i+1)+'</td><td>'+student[i]+'</td></tr>');
 }
-} 
-
-
-
-for(let i= 0;i<nums.length;i++){
-    let selected = minIndex(i,i+1);
-    if(i !== selected){
-        //交換i與selected處的元素
-        //換位子的程式
-        let tmp = nums[i];
-        nums[i] = nums[selected];
-        nums[selected] = tmp;
-    }
-}
-}
-
-let nums = [10 ,3 ,5 ,2 ,4 ];
-sort(nums);
-console.log(nums);
+document.write('</table>');
